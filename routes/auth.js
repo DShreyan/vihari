4e6a84afc136b119923e7536cd3df5f3a8bed2b9
Srikar4510 <srikar.a21@iiits.in>
2023-09-12 02:12:04 +0530

    reset

--- routes/auth.js
+++ routes/auth.js
@@ -9,5 +9,9 @@ router.get('/signup',authController.getsignup);
 router.post('/signup',authController.postsignup);
 router.post('/login',authController.postLogin);
 router.get('/logout',authController.Logout);
-router.post('/otp',authController.verifyEmail)
+router.post('/otp',authController.verifyEmail);
+router.get('/reset',authController.getReset);
+router.post('/reset',authController.postReset);
+router.get('/reset/:token',authController.getNewPassword);
+router.post('/newPassword',authController.postNewPassword);
 module.exports=router;
